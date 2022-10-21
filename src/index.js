988f6d285fce2bc425b67f790ddd0dbbc741343e
mhanifmustakim <mhanifmustakim@gmail.com>
2022-10-21 10:53:27 +0800

    Add styling for main project header page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Project from './Project.js';
 import './style.css';
 
 const homeProject = Project("Home");
-homeProject.setDescription("Your personal default project to store your todos :)");
+homeProject.setDescription("Your personal default project to store your todos.");
 Memory.addToProjects(homeProject);
 Memory.addToBookmarked(homeProject.id);
 
